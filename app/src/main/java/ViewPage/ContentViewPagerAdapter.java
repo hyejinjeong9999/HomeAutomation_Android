9f0b6fd50b4e032f917de355b2fdef323bfab691
vvvvvoin <aszx7009@gmail.com>
2020-05-06 17:42:43 +0900

    Update fragmentA, service

---
+++ app/src/main/java/ViewPage/ContentViewPagerAdapter.java
@@ -0,0 +1,61 @@
+package ViewPage;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class ContentViewPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragmentList;
+    private ArrayList<String> title;
+
+    public ContentViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        fragmentList = new ArrayList<>();
+//        fragmentList.add(new FragmentHome());
+//        fragmentList.add(new FragmentA());
+
+
+        title = new ArrayList<>();
+        title.add("HOME");
+        title.add("WIN");
+        title.add("냉장고");
+        title.add("현관문");
+        title.add("조명");
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title.get(position);
+    }
+
+    /**
+     * Posigion 에 위치한 Fragment 를 반환하는 Method
+     * ArrayList에 담아둔 Fragment 를 position index 를 통해 호출
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    /**
+     * page의 개수를 반환 => Fragment 배열의 크기가 page개수가 된다.
+     */
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    /**
+     * add Method를 통해 Fragment 배열에 추가할 Fragemnt 를 담는다.
+     * @param fragment
+     */
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+    }
+}
---
+++ app/src/main/java/ViewPage/FragmentA.java
@@ -0,0 +1,115 @@
+package ViewPage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.semiproject.R;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+import model.WeatherVO;
+
+public class FragmentA extends Fragment {
+    String TAG="FragmentA";
+    View view;
+    BufferedReader bufferedReader;
+    Context context;
+    TextView fragA_TV_01;
+    public FragmentA(){
+
+    }
+    public FragmentA(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_a,container,false);
+        context=container.getContext();
+
+        //fragA_TV_01  = view.findViewById(R.id.fragA_TV_01);
+        Intent i =  new Intent(context, Communication.WeatherService.class);
+        context.startService(i);
+
+        getActivity().startService(new Intent(getActivity(), Communication.WeatherService.class));
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    Bundle bundle = getArguments();
+                    if (bundle != null) {
+                        WeatherVO temperature = (WeatherVO) bundle.getSerializable("result");
+                        Log.i("test", temperature.getTemp());
+                        //fragA_TV_01.setText(temperature);
+                    }
+                }
+            }
+        };
+        Thread t = new Thread(r);
+        t.start();
+
+        return  view;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.v(TAG,"onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.v(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.v(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.v(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.v(TAG,"onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,"onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.v(TAG,"onDetach");
+    }
+}
