eb5cdc19faaa4e2e2dd8c4afad8156a3a0affeed
vz4repo <thevzforalt@gmail.com>
2020-05-06 10:45:00 +0900

    Merge manually dev+kwon

--- kwon/android/app/src/main/java/RecyclerViewAdapter/ViewType.java
+++ kwon/android/app/src/main/java/RecyclerViewAdapter/ViewType.java
@@ -1,5 +1,9 @@
 package RecyclerViewAdapter;
 
+/**
+ *  RecyclerVIew Item을 구별하기 위해 ViewType을 상수화 처리해서 사용
+ */
 public class ViewType {
     public static final int ItemVertical = 0;
+    public static final int ItemVerticalSwitch = 1;
 }
--- kwon/android/app/src/main/java/ViewPage/FragmentA.java
+++ kwon/android/app/src/main/java/ViewPage/FragmentA.java
@@ -2,6 +2,7 @@ package ViewPage;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,8 @@ import androidx.fragment.app.Fragment;
 
 import com.example.semiproject.R;
 
-// 날씨 관련
 public class FragmentA extends Fragment {
+    String TAG="FragmentA";
     View view;
     Context context;
 
@@ -24,6 +25,52 @@ public class FragmentA extends Fragment {
         context=container.getContext();
 
         return  view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.v(TAG,"onActivityCreated");
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.v(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.v(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.v(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.v(TAG,"onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG,"onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.v(TAG,"onDetach");
     }
 }
