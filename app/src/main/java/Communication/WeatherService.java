dc77b192f674df7cc01639e67fed83ec92f9872a
vvvvvoin <aszx7009@gmail.com>
2020-05-12 13:07:31 +0900

    tmp

--- app/src/main/java/Communication/WeatherService.java
+++ app/src/main/java/Communication/WeatherService.java
@@ -57,7 +57,7 @@ public class WeatherService extends Service {
 
         @Override
         public void run() {
-            String url = "http://70.12.229.165:8080/HomeAutomationServer/Weather";
+            String url = "http://70.12.60.98:8080/HomeAutomationWebServer/getWeather";
             try{
                 //1. URL 객체 생성
                 URL obj = new URL(url);
